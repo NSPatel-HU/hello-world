b052cc34236db0df92de20c822f212cabe5eee4e
Nishit <NSPatel2@my.harrisburgu.edu>
2023-02-18 11:01:28 -0500

    test github push trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//trigger jenkins pipeline
+//test trigger jenkins pipeline
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
